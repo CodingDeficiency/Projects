104d4dcec7b9a151ee4d7bbbc7a00a745667b1ea
CodingDeficiency <59585908+CodingDeficiency@users.noreply.github.com>
2026-02-25 11:12:38 -0600

    home page work

--- Portfolio/frontend/src/app/app.routes.ts
+++ Portfolio/frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Projects } from './components/projects/projects';
+import { ProjectDetails } from './components/project-details/project-details';
+import { Contact } from './components/contact/contact';
+import { Resume } from './components/resume/resume';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home }, // Home page
+  { path: 'portfolio', component: Projects }, // Portfolio of all projects
+  { path: 'project/:id', component: ProjectDetails }, // Individual project details
+  { path: 'contact', component: Contact }, // Contact page
+  { path: 'resume', component: Resume }, // Resume page
+  { path: '**', redirectTo: '' } // Redirect unknown routes to home
+];
